08a60f8784d91fecbf4891fdf37181e3e5325854
Oscar Otero <oom@oscarotero.com>
2024-02-28 18:44:39 +0100

    update hono

--- deps/hono.ts
+++ deps/hono.ts
@@ -1,7 +1,7 @@
-export { Hono } from "https://deno.land/x/hono@v4.0.7/mod.ts";
+export { Hono } from "https://deno.land/x/hono@v4.0.8/mod.ts";
 export {
   basicAuth,
   serveStatic,
-} from "https://deno.land/x/hono@v4.0.7/middleware.ts";
+} from "https://deno.land/x/hono@v4.0.8/middleware.ts";
 
-export type { Context, Next } from "https://deno.land/x/hono@v4.0.7/mod.ts";
+export type { Context, Next } from "https://deno.land/x/hono@v4.0.8/mod.ts";
