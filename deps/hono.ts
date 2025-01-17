ee26ae9fa4f98cb5ed9d4afb684635aec40852af
Oscar Otero <oom@oscarotero.com>
2025-01-17 20:53:53 +0100

    updated deps

--- deps/hono.ts
+++ deps/hono.ts
@@ -1,5 +1,5 @@
-export { Hono } from "jsr:@hono/hono@4.6.15";
-export { HTTPException } from "jsr:@hono/hono@4.6.15/http-exception";
-export { basicAuth } from "jsr:@hono/hono@4.6.15/basic-auth";
-export { serveStatic } from "jsr:@hono/hono@4.6.15/deno";
-export type { Context, Next } from "jsr:@hono/hono@4.6.15";
+export { Hono } from "jsr:@hono/hono@4.6.16";
+export { HTTPException } from "jsr:@hono/hono@4.6.16/http-exception";
+export { basicAuth } from "jsr:@hono/hono@4.6.16/basic-auth";
+export { serveStatic } from "jsr:@hono/hono@4.6.16/deno";
+export type { Context, Next } from "jsr:@hono/hono@4.6.16";
--- deps/std.ts
+++ deps/std.ts
@@ -5,7 +5,7 @@ export {
 } from "jsr:@std/yaml@1.0.5";
 export { test as testFrontMatter } from "jsr:@std/front-matter@1.0.5";
 export { extract as extractFrontMatter } from "jsr:@std/front-matter@1.0.5/yaml";
-export { emptyDir, ensureDir, expandGlob } from "jsr:@std/fs@1.0.8";
+export { emptyDir, ensureDir, expandGlob } from "jsr:@std/fs@1.0.9";
 export * as posix from "jsr:@std/path@1.0.8/posix";
 export {
   basename,
@@ -16,7 +16,7 @@ export {
   relative,
   SEPARATOR,
 } from "jsr:@std/path@1.0.8";
-export { format as formatBytes } from "jsr:@std/fmt@1.0.3/bytes";
+export { format as formatBytes } from "jsr:@std/fmt@1.0.4/bytes";
 export { decodeBase64, encodeBase64 } from "jsr:@std/encoding@1.0.6/base64";
 export { contentType } from "jsr:@std/media-types@1.1.0/content-type";
-export * as logger from "jsr:@std/log@0.224.12";
+export * as logger from "jsr:@std/log@0.224.13";
