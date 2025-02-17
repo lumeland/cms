1c31bc883253e5a8e99f4d9539f5cbd2060ece6b
Wesley Clements <wesley.clements@gmail.com>
2025-02-17 11:06:04 -0500

    simplified implementation by reverting back to merged type as default and then stripping properties away

--- types.ts
+++ types.ts
@@ -78,23 +78,6 @@ type Prettify<T> =
   }
   & {};
 
-/**
- * Converts a union type to an intersection type.
- *
- * @template U - The union type to convert
- * @returns An intersection of all types in the union
- *
- * @example
- * type Union = { a: string } | { b: number };
- * type Result = UnionToIntersection<Union>; // { a: string } & { b: number }
- *
- * @remarks
- * This uses conditional types and function parameter inference to achieve the conversion.
- * The resulting type will contain all members from each type in the union.
- */
-type UnionToIntersection<U> = (U extends any ? (x: U) => void : never) extends
-  ((x: infer I) => void) ? I : never;
-
 type Option = string | { value: string | number; label: string };
 type FieldTypes = FieldKeys | (string & Record<never, never>);
 
@@ -104,69 +87,69 @@ type FieldTypes = FieldKeys | (string & Record<never, never>);
 type FieldString = `${string}:${"" | " "}${FieldTypes}${"" | "!"}`;
 
 /**
- * Maps the field type to its unique options
+ * The common options for all fields.
  */
-type UniqueFieldOptionMap = Prettify<
-  Pick<
-    { [key in FieldKeys]: {} } & {
-      "checkbox": {
-        value?: boolean;
-      };
-      "choose-list": {
-        fields?: Field[];
-      };
-      "file": {
-        /** @deprecated. Use `upload` instead */
-        uploads?: string;
-        upload?: string | false;
-        publicPath?: string;
-      };
-      "list": {
-        options?: Option[];
-      };
-      "markdown": {
-        /** @deprecated. Use `upload` instead */
-        uploads?: string;
-        upload?: string | string[] | false;
-      };
-      "object": {
-        fields?: Field[];
-      };
-      "object-list": {
-        fields?: Field[];
-      };
-      "radio": {
-        options?: Option[];
-      };
-      "select": {
-        options?: Option[];
-      };
-    },
-    FieldKeys
-  >
->;
+type CommonFieldOptionsProperties =
+  | "name"
+  | "label"
+  | "value"
+  | "description"
+  | "view"
+  | "attributes"
+  | "init"
+  | "transform";
+
+/**
+ * Maps field types to the common properties to exclude.
+ */
+type FieldTypePropertyFilterMap = {
+  "choose-list": "value" | "attributes" | "init" | "transform";
+  "hidden":
+    | "label"
+    | "description"
+    | "view"
+    | "attributes"
+    | "init"
+    | "transform";
+  "list": "value" | "attributes" | "init" | "transform";
+  "object": "value" | "attributes" | "init" | "transform";
+  "object-list": "value" | "attributes" | "init" | "transform";
+};
+
+/**
+ * Maps field types to any additional properties they may have beyond the common ones.
+ */
+type FieldTypePropertyAdditionMap = {
+  "choose-list": "fields";
+  "file": "uploads" | "upload" | "publicPath";
+  "list": "options";
+  "markdown": "uploads" | "upload";
+  "object": "fields";
+  "object-list": "fields";
+  "radio": "options";
+  "select": "options";
+};
 
 /**
  * Maps the field type to a subset of options if it has one
  */
-type FieldOptionPropertySelectionMap = Prettify<
-  {
-    "choose-list": "name" | "label" | "description" | "view";
-    "hidden": "name" | "value";
-    "list": "name" | "label" | "description" | "view";
-    "object": "name" | "label" | "description" | "view";
-    "object-list": "name" | "label" | "description" | "view";
-  }
->;
+type FieldTypePropertySelectionMap = {
+  [K in FieldKeys]:
+    | (K extends keyof FieldTypePropertyFilterMap
+      ? Exclude<CommonFieldOptionsProperties, FieldTypePropertyFilterMap[K]>
+      : CommonFieldOptionsProperties)
+    | (K extends keyof FieldTypePropertyAdditionMap
+      ? FieldTypePropertyAdditionMap[K]
+      : never);
+};
 
 /**
  * Represents common field options shared by all field types.
  */
-interface CommonFieldOptions {
+export interface MergedFieldOptions {
   name: string;
   label?: string;
   description?: string;
-  value?: unknown;
   view?: string;
   attributes?: {
     required?: boolean;
@@ -177,22 +160,44 @@ interface CommonFieldOptions {
     pattern?: string;
     [key: string]: unknown;
   };
+  /** @deprecated. Use `upload` instead */
+  uploads?: string;
+  upload?: string | false;
+  publicPath?: string;
+  options?: Option[];
+  fields?: Field[];
   init?: (field: ResolvedField, content: CMSContent) => void | Promise<void>;
   transform?(value: any, field: ResolvedField): any;
 }
 
 /**
+ * Contains the mapping of field types to the type of their value property.
+ */
+type FieldOptionValueTypeMap = Prettify<
+  {
+    [K in FieldKeys]: Extract<FieldTypePropertySelectionMap[K], "value"> extends
+      never ? never : unknown;
+  } & {
+    "checkbox": boolean;
+    "number": number;
+  }
+>;
+
+/**
  * Creates the field options for one of the built-in field types with type `K`.
  */
-type BuiltInFieldOptions<K extends FieldKeys> = Prettify<
+type BuiltInFieldOptions<K extends FieldKeys> =
   & {
     type: K;
   }
-  & (K extends keyof FieldOptionPropertySelectionMap
-    ? Pick<CommonFieldOptions, FieldOptionPropertySelectionMap[K]>
-    : CommonFieldOptions)
-  & UniqueFieldOptionMap[K]
->;
+  & (FieldOptionValueTypeMap[K] extends never ? {}
+    : {
+      value?: FieldOptionValueTypeMap[K];
+    })
+  & Pick<
+    MergedFieldOptions,
+    Exclude<FieldTypePropertySelectionMap[K], "value">
+  >;
 
 /**
  * Represents the options for a custom field type.
@@ -204,7 +209,7 @@ type CustomFieldOptions = Prettify<
     //                  https://x.com/diegohaz/status/1524257274012876801
     [key: string]: unknown;
   }
-  & CommonFieldOptions
+  & MergedFieldOptions
 >;
 
 /**
@@ -212,8 +217,8 @@ type CustomFieldOptions = Prettify<
  */
 type FieldOptions = Prettify<
   | {
-    [K in keyof UniqueFieldOptionMap]: BuiltInFieldOptions<K>;
-  }[keyof UniqueFieldOptionMap]
+    [K in FieldKeys]: BuiltInFieldOptions<K>;
+  }[FieldKeys]
   | CustomFieldOptions
 >;
 
@@ -222,13 +227,11 @@ type FieldOptions = Prettify<
  */
 export type Field = FieldString | FieldOptions;
 
-/**
- * Combines all possible field options into a single type. (This is equivalent to the previous Field type)
- */
 export type MergedField = Prettify<
-  & { type: FieldOptions["type"] }
-  & CommonFieldOptions
-  & UnionToIntersection<UniqueFieldOptionMap[keyof UniqueFieldOptionMap]>
+  & {
+    type: FieldOptions["type"];
+  }
+  & MergedFieldOptions
 >;
 
 export type ResolvedField = Prettify<
